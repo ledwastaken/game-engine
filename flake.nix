10a53d473d772abb96b13c6d2315cb89e7f30680
ledwastaken <ledwastaken@proton.me>
2025-12-30 10:06:59 +0100

    chore(flake): add vulkan dependencies

--- flake.nix
+++ flake.nix
@@ -28,15 +28,28 @@
               rustfmt
               rust-analyzer
               libxkbcommon
+              vulkan-loader
+              vulkan-headers
+              vulkan-tools
+              vulkan-validation-layers
+              mesa
               pkg-config
               bashInteractive
             ] ++ x11Libs;
 
           shellHook = ''
             export SHELL=${pkgs.bashInteractive}/bin/bash
+            export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
             export LD_LIBRARY_PATH=${
               pkgs.lib.makeLibraryPath (
-                x11Libs ++ [ pkgs.libxkbcommon ]
+                x11Libs ++ [
+                  pkgs.vulkan-loader
+                  pkgs.vulkan-headers
+                  pkgs.vulkan-tools
+                  pkgs.vulkan-validation-layers
+                  pkgs.mesa
+                  pkgs.libxkbcommon
+                ]
               )
             }:$LD_LIBRARY_PATH
           '';
